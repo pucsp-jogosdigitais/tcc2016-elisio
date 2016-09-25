73fafbca2eaaa40083d5daf481ec66df1c152021
Jamison Duarte Da Silva <jamisonduarte@yahoo.com.br>
2016-09-25 03:11:28 -0300

    Migrando Assets Do Projeto Elisio

---
+++ Source/Elisio_01/Elisio_01.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Elisio_01.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Elisio_01, "Elisio_01" );
---
+++ Source/Elisio_01/Elisio_01GameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Elisio_01.h"
+#include "Elisio_01GameMode.h"
+
+
+
+
